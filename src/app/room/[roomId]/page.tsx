b51f3e9039a07e06bcaed7b1be23aa624859df0d
Khabir <hakim1khabir2@gmail.com>
2024-07-31 17:08:42 +0100

    [work] add video source to the page, add mic and video controls

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -2,34 +2,57 @@
 import { RoomManager } from "@/app/_lib/room.services";
 import ShareButton from "@/components/shareButton";
 import User from "@/components/User";
-import { MicIcon, VideoIcon } from "lucide-react";
+import { MicIcon, MicOff, VideoIcon, VideoOff } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ImPhoneHangUp } from "react-icons/im";
-
 
 export default function Room({ params }: { params: { roomId: string } }) {
   const roomURL = process.env.NEXT_PUBLIC_URL + "/room/" + params.roomId;
   const roomId = params.roomId;
   const user = useSession().data?.user;
-  // todo:  create room on mount
+  const [stream, setStream] = useState<MediaStream | undefined>(undefined);
+  const [cameraState, setCameraState] = useState<true | false>(false);
+  const [micState, setMicState] = useState<true | false>(false);
+
+  async function toggleCamera() {
+    let videoTrack = stream
+      ?.getTracks()
+      .find((track) => track.kind === "video");
+    if (videoTrack) {
+      videoTrack.enabled = !videoTrack.enabled;
+      setCameraState(videoTrack.enabled);
+    }
+  }
+  async function toggleMic() {
+    let micTrack = stream?.getTracks().find((track) => track.kind === "audio");
+    if (micTrack) {
+      micTrack.enabled = !micTrack.enabled;
+      setMicState(micTrack.enabled);
+    }
+  }
+
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    }).then((stream)=>{
-      const roomManager = new RoomManager(roomId, stream);
-      roomManager.createRoom();
-    })
-    console.log("create room");
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        //   {
+        //   width: {min: 640, ideal: 1920, max: 1920},
+        //   height: {min: 400, ideal: 1080, max: 1080},
+        // },
+        audio: true,
+      })
+      .then((stream) => {
+        const roomManager = new RoomManager(roomId, stream);
+        roomManager.createRoom();
+        setStream(roomManager.localStream);
+      });
   }, []);
   return (
     <div className="flex flex-col h-screen bg-gray-900 text-white">
-      <main className="flex flex-col items-center justify-center flex-1 w-full h-full bg-gray-900 text-white">
-        <div className="flex flex-wrap w-full h-full">
-          <User name="khair" />
-          <User name="kha" />
-        </div>
+      <main className="flex flex-row items-center justify-center flex-1 w-full h-full flex-wrap gap-3 p-2 bg-gray-900 text-white">
+        <User name="khair" stream={stream} />
+        <User name="kha" />
       </main>
 
       <div className="p-4 bg-gray-800">
@@ -38,11 +61,25 @@ export default function Room({ params }: { params: { roomId: string } }) {
             <p>{roomId}</p>
           </div>
           <div className="flex-1 space-x-4 flex justify-center">
-            <button className="p-3 rounded-full text-white bg-gray-700">
-              <MicIcon className="h-6 w-6" />
+            <button
+              onClick={() => toggleMic()}
+              className="p-3 rounded-full text-white bg-gray-700"
+            >
+              {micState ? (
+                <MicIcon className="h-6 w-6" />
+              ) : (
+                <MicOff className="h-6 w-6" />
+              )}
             </button>
-            <button className="p-3 rounded-full text-white bg-gray-700">
-              <VideoIcon className="h-6 w-6" />
+            <button
+              onClick={() => toggleCamera()}
+              className="p-3 rounded-full text-white bg-gray-700"
+            >
+              {cameraState ? (
+                <VideoIcon className="h-6 w-6" />
+              ) : (
+                <VideoOff className="h-6 w-6" />
+              )}
             </button>
             <button className="p-3 rounded-full bg-red-600">
               <ImPhoneHangUp className="h-6 w-6" />
@@ -56,4 +93,3 @@ export default function Room({ params }: { params: { roomId: string } }) {
     </div>
   );
 }
-
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,13 +1,38 @@
+"use client"
 import type { User } from "@/app/_lib/definitions";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export default function User({ name }: { name: string }) {
+export default function User({
+  name,
+  stream,
+}: {
+  name: string;
+  stream?: MediaStream;
+}) {
+  const refVideo = useRef<HTMLVideoElement>(null)
+  useEffect(() => {
+    if (!refVideo.current || !stream) return
+    refVideo.current.srcObject = stream
+  }, [stream])
   return (
+    <div>
+      
+    {stream ? (
+        <video
+          autoPlay
+          playsInline
+          ref={refVideo}
+          className="flex-1"
+        />
+    ) : (
+
     <div className="flex flex-col items-center justify-center grow">
       <div className="flex flex-col items-center justify-center bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white w-24 h-24 rounded-full m-4 shadow-lg transform hover:scale-105 transition-transform duration-300">
         <div className="text-4xl font-bold">{name![0].toUpperCase()}</div>
       </div>
       <div className="text-sm mt-2">{name}</div>
+    </div>
+    )}
     </div>
   );
 }
